3c6582c4543a9d884244ae4f1219b7adea23bf44
Kenny Dasilva <kennydasilva542@gmail.com>
2026-01-28 15:18:37 +0200

    addicion of ip model

---
+++ ip_tracking/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class RequestLog(models.Model):
+    ip_address=models.GenericIPAddressField()
+    path=models.CharField(max_length=255)
+    timestamp=models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.path}"
